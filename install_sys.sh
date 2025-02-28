d2d0185c95aa8b4319eddee8a2da75ceec4fdb97
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-02-28 04:02:39 +0000

    desperate times

--- install_sys.sh
+++ install_sys.sh
@@ -1,61 +1,66 @@
 #!/bin/bash
 
+# Never run pacman -Sy on your system!
 pacman -Sy dialog --noconfirm
 
 timedatectl set-ntp true
 
-dialog --defaultno --title "Are you Sure?" --yesno \
-    "This is my personal arch linux install. \n\n\
-    It will destroy everything on one of your hard disks. \n\n\
-    Do not say YES if you are not sure what you are doing. \n\n\
-    Do you want to continue?" 15 60 || exit
+# Welcome message of type yesno - see `man dialog`
+dialog --defaultno --title "Are you sure?" --yesno \
+"This is my personnal arch linux install. \n\n\
+It will just DESTROY EVERYTHING on the hard disk of your choice. \n\n\
+Don't say YES if you are not sure about what you're doing! \n\n\
+Are you sure?" 15 60 || exit
 
-# enter hostname
 dialog --no-cancel --inputbox "Enter a name for your computer." \
     10 60 2> comp
 
 comp=$(cat comp) && rm comp
 
-# verify the boot (UEFI or BIOS)
+# Verify boot (UEFI or BIOS)
 uefi=0
 ls /sys/firmware/efi/efivars 2> /dev/null && uefi=1
 
-# giving the user the choice of which hard disk to gormat
+# Choosing the hard drive
 devices_list=($(lsblk -d | awk '{print "/dev/" $1 " " $4 " on"}' \
     | grep -E 'sd|hd|vd|nvme|mmcblk'))
 
 dialog --title "Choose your hard drive" --no-cancel --radiolist \
-    "Where do you want to install your new system? \n\n\
-    Select with SPACE, validate with ENTER. \n\n\
-    WARNING: Everything will be DESTROYED on the hard disk!" \
-    15 60 4 "${devices_list[@]}" 2> hd
-    hd=$(cat hd) && rm hd
+"Where do you want to install your new system? \n\n\
+Select with SPACE, valid with ENTER. \n\n\
+WARNING: Everything will be DESTROYED on the hard disk!" \
+15 60 4 "${devices_list[@]}" 2> hd
 
-# Partitioning and installing Arch Linux
+hd=$(cat hd) && rm hd
+
+# Ask for the size of the swap partition
 default_size="8"
 dialog --no-cancel --inputbox \
-    "You need three partitions: Boot, Root and Swap \n\
-    The boot partition will be 512M \n\
-    The root partition will be the remaining of the hard disk \n\n\
-    Enter below the partition size (in Gb) for the Swap. \n\n\
-    If you do not enter anything, it will default to ${default_size}G. \n" \
-    20 60 2> swap_size
+"You need four partitions: Boot, Root and Swap \n\
+The boot partition will be 512M \n\
+The root partition will be the remaining of the hard disk \n\n\
+Enter below the partition size (in Gb) for the Swap. \n\n\
+If you don't enter anything, it will default to ${default_size}G. \n" \
+20 60 2> swap_size
+
 size=$(cat swap_size) && rm swap_size
 
 [[ $size =~ ^[0-9]+$ ]] || size=$default_size
 
-# ERASING the hard disk :)
 dialog --no-cancel \
-    --title "!!! DELETING EVERYTHING TIME !!!"\
-    --menu "Choose your weapon to wipe the hard disk ($hd)" \
-    15 60 4 \
-    1 "Use dd (wipe all disk)" \
-    2 "Use shred (slow & secure)" \
-    3 "No need - my hard disk is empty" 2> eraser
+--title "!!! DELETE EVERYTHING !!!" \
+--menu "Choose the way you'll wipe your hard disk ($hd)" \
+15 60 4 \
+1 "Use dd (wipe all disk)" \
+2 "Use schred (slow & secure)" \
+3 "No need - my hard disk is empty" 2> eraser
 
 hderaser=$(cat eraser); rm eraser
 
-#This function is for erasing the disk either with dd or shred
+# This function can wipe out a hard disk.
+# DO NOT RUN THIS FUNCTION ON YOUR ACTUAL SYSTEM!
+# If you did it, DO NOT CALL IT!!
+# If you did it, I'm sorry.
 function eraseDisk() {
     case $1 in
         1) dd if=/dev/zero of="$hd" status=progress 2>&1 \
@@ -64,7 +69,7 @@ function eraseDisk() {
             --progressbox --stdout 20 60;;
         2) shred -v "$hd" \
             | dialog \
-            --title "Formatting $hd.." \
+            --title "Formatting $hd..." \
             --progressbox --stdout 20 60;;
         3) ;;
     esac
@@ -72,13 +77,11 @@ function eraseDisk() {
 
 eraseDisk "$hderaser"
 
-# Creating Partitions - to clear confusions, fdisk uses numbers for boot type
-# 4 is for BIOS and UEFI is 1, could define variable uefi as 4 then change to 1 but idk
 boot_partition_type=1
 [[ "$uefi" == 0 ]] && boot_partition_type=4
 
 # Create the partitions
-#Be careful with this bit, the spaces are there for a reason..
+
 #g - create non empty GPT partition table
 #n - create new partition
 #p - primary partition
@@ -108,7 +111,7 @@ EOF
 
 partprobe "$hd"
 
-# Add a suffix "p" in case we have a NVMe controller chip
+# Add a suffix "p" in case with have a NVMe controller chip
 echo "$hd" | grep -E 'nvme' &> /dev/null && hd="${hd}p"
 
 # Format the partitions
@@ -123,7 +126,7 @@ if [ "$uefi" = 1 ]; then
     mount "${hd}1" /mnt/boot/efi
 fi
 
-# Install Arch Linux!
+# Install Arch Linux! Glory and fortune!
 pacstrap /mnt base base-devel linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 
@@ -132,8 +135,9 @@ echo "$uefi" > /mnt/var_uefi
 echo "$hd" > /mnt/var_hd
 mv comp /mnt/comp
 
-curl https://raw.githubusercontent.com/ForgottenScream\
-/arch_installer/main/install_chroot.sh > /mnt/install_chroot.sh
+# Don't forget to replace "Phantas0s" by the username of your Github account
+curl https://raw.githubusercontent.com/Phantas0s\
+/arch_installer/master/install_chroot.sh > /mnt/install_chroot.sh
 
 arch-chroot /mnt bash install_chroot.sh
 
@@ -143,7 +147,7 @@ rm /mnt/install_chroot.sh
 rm /mnt/comp
 
 dialog --title "To reboot or not to reboot?" --yesno \
-"The install is completed. \n\n\
+"Congrats! The install is done! \n\n\
 Do you want to reboot your computer?" 20 60
 
 response=$?
