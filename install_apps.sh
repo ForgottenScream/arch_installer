2f26150d2c58bff5ef2243ce44b6b4dcedba892d
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-02-28 04:38:02 +0000

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -4,7 +4,6 @@ name=$(cat /tmp/user_name)
 
 apps_path="/tmp/apps.csv"
 
-# Don't forget to replace "Phantas0s" by the username of your Github account
 curl https://raw.githubusercontent.com/ForgottenScream\
 /arch_installer/main/apps.csv > $apps_path
 
